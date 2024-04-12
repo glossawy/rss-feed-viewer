6e16631189113abb1ad0884d803c166172e05192
Glossawy <glossawy@sphorb.email>
2024-04-12 14:25:28 -0400

    initial setup

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks'
+import eslintImport from 'eslint-plugin-import'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tsEslint.config({
+  files: ['**/*.ts', '**/*.tsx'],
+  ignores: ['dist/**/*'],
+  extends: [
+    eslint.configs.recommended,
+    ...tsEslint.configs.recommended,
+    prettierConfig,
+  ],
+  plugins: {
+    'react-refresh': reactRefresh,
+    import: eslintImport,
+    'react-hooks': reactHooks,
+  },
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.es2020,
+    },
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    semi: ['error', 'never'],
+    'sort-imports': [
+      'warn',
+      { ignoreDeclarationSort: true, allowSeparatedGroups: true },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+        ],
+        pathGroups: [
+          { pattern: '@app', group: 'internal' },
+          { pattern: '@mantine', group: 'external' },
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+})
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import App from '@app/App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
